9307ac935c8cff2fb032ebcbbdab0241e8cfe107
vaibhav7024 <vjaiswal7024@gmail.com>
2025-09-19 21:14:48 +0530

    Create solve.cpp

---
+++ solve.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1 || s.size()<=numRows) return s;
+
+        vector<string> rows(s.size());
+        int curr=0;
+        bool dir=true;
+        for(char ch:s){
+            rows[curr]+=ch;
+            if(curr==0 || curr==numRows-1) dir=!dir;
+            curr+= dir?-1:1;
+        }
+        string res="";
+        for(string ch:rows){
+            res+=ch;
+        }
+        return res;
+    }
+};
